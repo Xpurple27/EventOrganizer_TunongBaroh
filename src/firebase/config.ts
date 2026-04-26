b937a759aea5231de917c7d99c6fd3779502b1c3
Wafiq Afnsa <afansa27@gmail.com>
2026-04-26 07:25:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8609158229-32769",
+  "appId": "1:590474290384:web:434314aec8e6603e2e6ad5",
+  "apiKey": "AIzaSyBCS-KrzMbm2JV6LsgwkVXjukjt7GwsJEo",
+  "authDomain": "studio-8609158229-32769.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "590474290384"
+};
